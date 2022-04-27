7238c39d5fef46251279404a466880bad8193bb8
carys <caryspritch@gmail.com>
2022-04-27 11:53:20 +0100

    fixed zoom on heat layer

--- map.js
+++ map.js
@@ -4,105 +4,52 @@ var bounds = [[0,0],[1000,1000]];
 var image = L.imageOverlay('floor.png', bounds).addTo(map);
 map.fitBounds(bounds);
 
-
-
-
 // Change us to vary the base sizes of the heatmap points.
-const defaultHmRadius = 40;
-const defaultHmBlur = 25;
+let defaultHmRadius = 40;
+let defaultHmBlur = 25;
 const gradient = {0.4: 'blue', 0.55: 'lime', 0.80: 'yellow', 1: 'red'}
 
+var hmRadius = defaultHmRadius;
+var hmBlur = defaultHmBlur;
 
-var heat = L.heatLayer([], {radius: defaultHmRadius, blur: defaultHmBlur, gradient:gradient }).addTo(map);
+// if zoomend 
 
+myfunc = function (){
+    if (('zoomend' )) {
 
-// We don't want the heatmap to stay the same size as we zoom in and out, we 
-// want it to scale with us. This provides a naive implementation of such a 
-// function (comment out to see the default functionality).
-map.on('zoomend', function(ev) {
-    // To avoid multiplying by zero (remove the + 1 and zoom in and out to see 
-    // what happens otherwise)
-    let multiplier = map.getZoom() + 1;
+    (map.on('zoomend',  func = function (e ) {
 
-    // This probably wants to be a little more sophisticated than just a linear
-    // scaling.
-    defaultHmRadius * multiplier;
-    defaultHmBlur * multiplier;
-
-    // Update the heatmap
-    heat.setOptions({radius: defaultHmRadius, blur: defaultHmBlur});
-});
-
-var LightIcon = L.Icon.extend({
-    options: {
-    iconSize:[60, 55],
-    }
-});
-
-var LightIcon = new LightIcon ({
-    iconUrl: 'light.png'});
-
-// Given the requisite coordinates and a value, create a marker and a heatmap
-// point
-function createPointOne(map, heatmap, latlng, dataPointOne)
-{
-    // A point on the heatmap consists of three values - the latitude, the
-    // longitude, and the intensity of the point, whereas a normal marker is
-    // just the latitude and longitude
-    var latlngint = latlng;
-    latlngint.push(dataPointOne);
-
-    var marker = L.marker(latlng,{icon: LightIcon}).addTo(map);
+        let multiplier = map.getZoom() + 1;
     
-    heatmap.addLatLng(latlng);
-    return marker;
+        defaultHmRadius * multiplier;
+        defaultHmBlur *  multiplier;
+        
+    
+        
+        hmRadius = defaultHmRadius * multiplier;
+        hmBlur = defaultHmBlur * multiplier;
+    
+        return heat.setOptions({radius: hmRadius, blur: hmBlur});
+    }))};
 }
 
-
-function createPointTwo(map, heatmap, latlng, dataPointTwo)
-{
-    // A point on the heatmap consists of three values - the latitude, the
-    // longitude, and the intensity of the point, whereas a normal marker is
-    // just the latitude and longitude
-    var latlngint = latlng;
-    latlngint.push(dataPointTwo);
+myfunc(heat);
+var heat = L.heatLayer([], {radius: defaultHmRadius, blur: defaultHmBlur }).addTo(map);
 
 
-    var marker = L.marker(latlng,{icon: LightIcon}).addTo(map);
-    
-    heatmap.addLatLng(latlng);
-    return marker;
-}
+var heat = L.heatLayer([], {radius: hmRadius, blur: hmBlur}).addTo(map);
 
 
 function createPointThree(map, heatmap, latlng, dataPointThree)
 {
-    // A point on the heatmap consists of three values - the latitude, the
-    // longitude, and the intensity of the point, whereas a normal marker is
-    // just the latitude and longitude
     var latlngint = latlng;
     latlngint.push(dataPointThree);
 
 
-    var marker = L.marker(latlng,{icon: LightIcon}).addTo(map);
+    var marker = L.marker(latlng,).addTo(map);
     
     heatmap.addLatLng(latlng);
     return marker;
 }
 
-// Supposedly the highest intensity value by default should be 1.0, but that 
-// doesn't work for me
-// createPointOne(map, heat, [800,800], dataPointOne);
-
-
-createPointOne(map, heat, [400,400], dataPointOne);
-createPointTwo(map, heat, [120,120], dataPointTwo);
 createPointThree(map, heat, [500,300], dataPointThree);
-
-
-function removeClass(heat){
-    var off = heat;
-    off.classList.remove(heat);
-}
-
-removeClass(heat);
